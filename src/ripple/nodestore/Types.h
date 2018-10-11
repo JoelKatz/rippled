b2f164983cb34e02804cf3a7088eb63aac38f8d7
JoelKatz <DavidJoelSchwartz@GMail.com>
2018-10-11 11:11:17 -0700

    Control memory growth from slow writes
    * Don't allow a write batch to grow without bound
    * Don't fetch history if write load is high

--- src/ripple/nodestore/Types.h
+++ src/ripple/nodestore/Types.h
@@ -32,7 +32,13 @@ enum
     // This is only used to pre-allocate the array for
     // batch objects and does not affect the amount written.
     //
-    batchWritePreallocationSize = 256
+    batchWritePreallocationSize = 256,
+
+    // This sets a limit on the maximum number of writes
+    // in a batch. Actual usage can be twice this since
+    // we have a new batch growing us we write the lod.
+    //
+    batchWriteLimitSize = 65536
 };
 
 /** Return codes from Backend operations. */
